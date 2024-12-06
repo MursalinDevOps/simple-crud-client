86e5b1101e5d8112438ad1e419eb548b14dc9ea8
MursalinDevOps <mursalin1768@gmail.com>
2024-12-06 23:49:21 +0600

    delete user req sent to the server

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,14 +3,28 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
     const users = useLoaderData();
     // console.log(users)
+
+    const handleDelete = (id) => {
+        console.log(id);
+        fetch(`http://localhost:5000/users/${id}`, {
+            method:'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
     return (
         <div>
             <h2>Total users - {users.length}</h2>
             <div>
                 {
-                    users.map(user => <div key={user._id}>
+                    users.map(user => <div style={{border:'1px solid', margin:'10px', padding:'20px'}} key={user._id}>
+                        <p>UserId : {user._id}</p>
                         <p>Name : {user.name}</p>
-                        <p>Email : {user.email}</p> </div>)
+                        <p>Email : {user.email}</p> 
+                        <button onClick={()=>handleDelete(user._id)}>X</button>
+                        </div>)
                 }
             </div>
         </div>
