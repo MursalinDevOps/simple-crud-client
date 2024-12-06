2945552a43ace3a27b9cb5564d68816d21764994
MursalinDevOps <mursalin1768@gmail.com>
2024-12-07 00:05:36 +0600

    delete user functionality added

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -7,24 +7,29 @@ const Users = () => {
     const handleDelete = (id) => {
         console.log(id);
         fetch(`http://localhost:5000/users/${id}`, {
-            method:'DELETE'
+            method: 'DELETE'
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    alert('User has been deleted!')
+                }else{
+                    alert('Something went wrong! try again.')
+                }
+            })
     }
     return (
         <div>
             <h2>Total users - {users.length}</h2>
             <div>
                 {
-                    users.map(user => <div style={{border:'1px solid', margin:'10px', padding:'20px'}} key={user._id}>
+                    users.map(user => <div style={{ border: '1px solid', margin: '10px', padding: '20px' }} key={user._id}>
                         <p>UserId : {user._id}</p>
                         <p>Name : {user.name}</p>
-                        <p>Email : {user.email}</p> 
-                        <button onClick={()=>handleDelete(user._id)}>X</button>
-                        </div>)
+                        <p>Email : {user.email}</p>
+                        <button onClick={() => handleDelete(user._id)}>X</button>
+                    </div>)
                 }
             </div>
         </div>
