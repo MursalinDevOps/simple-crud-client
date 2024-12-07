02b4a3ef3f71a3c72fd8bed57ae323355ab9c832
MursalinDevOps <mursalin1768@gmail.com>
2024-12-07 19:20:33 +0600

    user info update and show alert added

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -3,19 +3,46 @@ import { useLoaderData } from "react-router-dom";
 const Update = () => {
     const userData = useLoaderData();
     console.log(userData)
+    const handleUpdate = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const updatedUserInfo = { name, email };
+        console.log(name, email)
+
+        fetch(`http://localhost:5000/users/${userData._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUserInfo)
+        }
+        )
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0 ) {
+                alert('User info updated !')
+                }
+            })
+    }
+
     return (
-        <div style={{textAlign:'center', border:'1px solid'}}>
+        <div style={{ textAlign: 'center', border: '1px solid' }}>
             <h1>Update information of <u>{userData.name}</u></h1>
             {/* <p>ID : {userData._id}</p>
             <h3>Name : {userData.name}</h3>
             <h3>Email : {userData.email}</h3> */}
-            <input type="text" name="name" placeholder="Name"/>
-            <br />
-            <br />
-            <input type="email" name="email" placeholder="Email"/>
-            <br />
-            <br />
-            <input type="submit" value="Update" style={{backgroundColor:'blue', color:'white', fontWeight:'bold', padding:'5px 20px', marginBottom:'20px'}} />
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" defaultValue={userData?.name} />
+                <br />
+                <br />
+                <input type="email" name="email" defaultValue={userData?.email} />
+                <br />
+                <br />
+                <input type="submit" value="Update" style={{ backgroundColor: 'blue', color: 'white', fontWeight: 'bold', padding: '5px 20px', marginBottom: '20px' }} />
+            </form>
         </div>
     );
 };
