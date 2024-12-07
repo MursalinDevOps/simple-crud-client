b678b15e1f7195dfbfeb33697a54af4527ba18a8
MursalinDevOps <mursalin1768@gmail.com>
2024-12-07 18:24:48 +0600

    update info form added

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -5,9 +5,17 @@ const Update = () => {
     console.log(userData)
     return (
         <div style={{textAlign:'center', border:'1px solid'}}>
-            <p>ID : {userData._id}</p>
+            <h1>Update information of <u>{userData.name}</u></h1>
+            {/* <p>ID : {userData._id}</p>
             <h3>Name : {userData.name}</h3>
-            <h3>Email : {userData.email}</h3>
+            <h3>Email : {userData.email}</h3> */}
+            <input type="text" name="name" placeholder="Name"/>
+            <br />
+            <br />
+            <input type="email" name="email" placeholder="Email"/>
+            <br />
+            <br />
+            <input type="submit" value="Update" style={{backgroundColor:'blue', color:'white', fontWeight:'bold', padding:'5px 20px', marginBottom:'20px'}} />
         </div>
     );
 };
